08d310caaf299c107898a3af1944bff4fd449272
Macsim Minakov <maksim.minakov.83@mail.ru>
2025-01-11 22:17:03 +0300

    1 back end курс simple express app with typescript and nodemon

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-const express = require('express')
+import express, {Request, Response} from 'express';
 const app = express()
 const port = 3000
 
-app.get('/', (req: any, res: any) => {
-    const helloWorld = 'Hello World?';
+app.get('/', (req: Request, res: Response) => {
+    const helloWorld = 'Hello World';
     res.send(helloWorld)
 })
 
